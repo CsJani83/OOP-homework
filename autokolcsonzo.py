65eace09c703108f5581f88f68cdd666859c82e7
Csomós János <csomosjanos1983@gmail.com>
2025-05-31 16:25:38 +0200

    berles.py

--- autokolcsonzo.py
+++ autokolcsonzo.py
@@ -1,6 +1,6 @@
 from szemelyauto import Szemelyauto
 from teherauto import Teherauto
-from berles import Berles  # Most már a Berles osztályt is importáljuk
+from berles import BerlesKezelo  # Importáljuk az új BerlesKezelo osztályt
 
 class Autokolcsonzo:
     def __init__(self, nev):
@@ -10,55 +10,16 @@ class Autokolcsonzo:
             Szemelyauto("SZA-002", "VW", 10000),
             Teherauto("TEA-001", "Iveco", 15000)
         ]
-        
-        # **Előre betöltött bérlések, most már Berles objektumokkal**
-        self.berlesek = {
-            "SZA-001": [Berles("Kiss Ádám", self.autok[0], "2025-07-01", 1)],
-            "SZA-002": [Berles("Nagy Éva", self.autok[1], "2025-07-02", 2), Berles("Ádám Éva", self.autok[1], "2025-07-04", 1)],
-            "TEA-001": [Berles("Gipsz Jakab", self.autok[2], "2025-07-01", 1)]
-        }
+        self.berleskezelo = BerlesKezelo()  # Most már az új osztály tárolja a bérléseket
 
     def berel_auto(self, berlo, rendszam, datum, napok_szama):
-        """
-        Autó bérlése adott bérlőnek, dátumra és napok számával.
-        """
-        if rendszam in self.berlesek:
-            for foglalas in self.berlesek[rendszam]:
-                if foglalas.datum == datum:
-                    return "Az autó már foglalt ezen a napon!"
-
         auto = next((a for a in self.autok if a.rendszam == rendszam), None)
         if not auto:
             return "Érvénytelen autórendszám!"
-
-        uj_berles = Berles(berlo, auto, datum, napok_szama)
-        if rendszam not in self.berlesek:
-            self.berlesek[rendszam] = []
-        self.berlesek[rendszam].append(uj_berles)
-
-        return f"Sikeres foglalás! {berlo} bérelte a(z) {rendszam} rendszámú {auto.get_auto_tipus()} {napok_szama} napra. Ár: {uj_berles.ar} Ft"
+        return self.berleskezelo.uj_berles(berlo, auto, datum, napok_szama)
 
     def lemond_berles(self, berlo):
-        """
-        Bérlés törlése adott bérlő esetén.
-        """
-        for rendszam, foglalasok in list(self.berlesek.items()):
-            for foglalas in foglalasok:
-                if foglalas.berlo == berlo:
-                    foglalasok.remove(foglalas)
-                    return f"{berlo} sikeresen lemondta a bérlést."
-        return "Nincs ilyen bérlés!"
+        return self.berleskezelo.lemond_berles(berlo)
 
     def listaz_berlesek(self):
-        """
-        Az összes aktuális bérlés kiírása.
-        """
-        if not self.berlesek:
-            return "Jelenleg nincs aktív bérlés."
-
-        kiiras = "\nAktuális bérlések:"
-        for rendszam, foglalasok in self.berlesek.items():
-            auto = next((a for a in self.autok if a.rendszam == rendszam), None)
-            for foglalas in foglalasok:
-                kiiras += f"\n{foglalas}"
-        return kiiras
+        return self.berleskezelo.listaz_berlesek()
--- berles.py
+++ berles.py
@@ -8,3 +8,31 @@ class Berles:
 
     def __str__(self):
         return f"{self.berlo} bérelte: {self.auto.rendszam} ({self.auto.get_auto_tipus()}) - {self.datum} ({self.napok_szama} nap) - Ár: {self.ar} Ft"
+
+
+class BerlesKezelo:
+    def __init__(self):
+        self.berlesek = []  # Minden bérlés egy listában tárolódik
+
+    def uj_berles(self, berlo, auto, datum, napok_szama):
+        # Ellenőrzés: Van-e már foglalás ezen a napon erre az autóra?
+        for berles in self.berlesek:
+            if berles.auto.rendszam == auto.rendszam and berles.datum == datum:
+                return "Az autó már foglalt ezen a napon!"
+
+        # Új bérlés hozzáadása
+        uj_berles = Berles(berlo, auto, datum, napok_szama)
+        self.berlesek.append(uj_berles)
+        return f"Sikeres foglalás! {uj_berles}"
+
+    def lemond_berles(self, berlo):
+        for berles in self.berlesek:
+            if berles.berlo == berlo:
+                self.berlesek.remove(berles)
+                return f"{berlo} sikeresen lemondta a bérlést."
+        return "Nincs ilyen bérlés!"
+
+    def listaz_berlesek(self):
+        if not self.berlesek:
+            return "Jelenleg nincs aktív bérlés."
+        return "\n".join(str(berles) for berles in self.berlesek)
